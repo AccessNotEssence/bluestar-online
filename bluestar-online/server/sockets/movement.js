ca44b993da4598f8243459aebc8cdd592292a83f
AccessNotEssence <bxg08051005@gmail.com>
2026-09-23 03:22:00 +0800

    Update movement.js

--- bluestar-online/server/sockets/movement.js
+++ bluestar-online/server/sockets/movement.js
@@ -1,55 +1,73 @@
-const { Server } = require('socket.io');
+const entities = {};
 
-const connectedEntities = {};
+module.exports = function(server) {
+  const { Server } = require('socket.io');
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
 
-function initSockets(server) {
-    const io = new Server(server, {
-        cors: {
-            origin: '*',
-            methods: ['GET', 'POST']
-        }
+  io.on('connection', (socket) => {
+    console.log(`[SOCKET CONNECTED] New client connected: ${socket.id}`);
+
+    // 1. Send existing entities to newly connected client
+    socket.emit('currentEntities', entities);
+
+    // 2. Handle join lounge
+    socket.on('joinLounge', (data) => {
+      entities[socket.id] = {
+        id: socket.id,
+        name: data.name || 'Officer',
+        type: data.type || 'HUMAN',
+        x: data.x || 400,
+        y: data.y || 320
+      };
+
+      console.log(`[ENTITY JOINED] ${entities[socket.id].name} (${entities[socket.id].type})`);
+
+      // Broadcast to ALL clients (including sender) that a new entity joined
+      io.emit('entityJoined', entities[socket.id]);
     });
 
-    io.on('connection', (socket) => {
-        console.log(`[WebSocket] Connected: ${socket.id}`);
+    // 3. Handle movement
+    socket.on('move', (data) => {
+      if (entities[socket.id]) {
+        entities[socket.id].x = data.x;
+        entities[socket.id].y = data.y;
 
-        socket.on('join_starship', (data) => {
-            connectedEntities[socket.id] = {
-                socketId: socket.id,
-                id: data.id,
-                name: data.displayName,
-                type: data.type || 'HUMAN',
-                avatarSprite: data.type === 'AGENT' ? 'robot_unit' : (data.avatarSprite || 'human_astronaut'),
-                x: data.x || 1000,
-                y: data.y || 1000
-            };
-
-            // Send existing entities to the newly joined entity
-            socket.emit('current_entities', connectedEntities);
-
-            // Broadcast newly joined entity to everyone else
-            socket.broadcast.emit('entity_joined', connectedEntities[socket.id]);
+        // Broadcast updated movement to all other clients
+        socket.broadcast.emit('entityMoved', {
+          id: socket.id,
+          x: data.x,
+          y: data.y
         });
-
-        socket.on('move', (pos) => {
-            if (connectedEntities[socket.id]) {
-                connectedEntities[socket.id].x = pos.x;
-                connectedEntities[socket.id].y = pos.y;
-
-                socket.broadcast.emit('entity_moved', {
-                    socketId: socket.id,
-                    x: pos.x,
-                    y: pos.y
-                });
-            }
-        });
-
-        socket.on('disconnect', () => {
-            console.log(`[WebSocket] Disconnected: ${socket.id}`);
-            delete connectedEntities[socket.id];
-            io.emit('entity_left', socket.id);
-        });
+      }
     });
-}
 
-module.exports = initSockets;
+    // 4. Handle chat messages (CRITICAL FIX: Broadcast back to everyone!)
+    socket.on('chatMessage', (msg) => {
+      const entity = entities[socket.id];
+      const senderName = entity ? entity.name : 'Unknown';
+
+      console.log(`[CHAT] [${senderName}]: ${msg}`);
+
+      // Broadcast the chat message to EVERYONE (including sender)
+      io.emit('chatMessage', {
+        id: socket.id,
+        name: senderName,
+        message: msg
+      });
+    });
+
+    // 5. Handle disconnection
+    socket.on('disconnect', () => {
+      console.log(`[SOCKET DISCONNECTED] Client disconnected: ${socket.id}`);
+      if (entities[socket.id]) {
+        delete entities[socket.id];
+        io.emit('entityLeft', socket.id);
+      }
+    });
+  });
+};
